8712f71a1df226d8f3356974229a7dfd77dde2ae
kebrcic <brcicsutton.kevin@gmail.com>
2024-10-16 21:48:18 -0600

    Revert "Database connection"
    This reverts commit 193c6ed10f4357986314d173fe0a149991b7efb1.

--- src/components/AuthPage.jsx
+++ src/components/AuthPage.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import Title from './Title';
 import FormGroup from './FormGroup';
 import Checkbox from './Checkbox';
-import supabase from '../config/supabaseClient';
 import './Login.css';
 
 const AuthPage = () => {
